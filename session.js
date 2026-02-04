6a5402ce53ba9798de6424843fa64324b70405b4
netmiherath123-del <netmiherath123@gmail.com>
2026-02-04 14:25:13 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1770126415339_2h8ffio7a",
 PORT: process.env.PORT || "8000"
 };
